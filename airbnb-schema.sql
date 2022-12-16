d79d24dcbdec9ae5ec7dac70cc9f47e69681b27d
Igor <68248740+richard96292@users.noreply.github.com>
2022-12-16 17:28:46 +0200

    Add views

--- airbnb-schema.sql
+++ airbnb-schema.sql
@@ -133,18 +133,32 @@ CREATE TABLE categories
     FOREIGN KEY (listing_id) REFERENCES listings (listing_id)
 );
 
---
--- View structure for view `user_list`
---
+### views
+# view with all non-sensitive information about active users
+create view active_user_list as
+select user_id, first_name, last_name, email, phone_number, profile_image
+from users
+where active = true;
 
---
--- View structure for view `property_list`
---
+# view with full info about a property
+create view full_property_info as
+select l.*, a.street, a.zip_code, a.latitude, a.longitude, c.city, c2.country
+from listings l
+         inner join addresses a on l.address_id = a.address_id
+         inner join cities c on a.city_id = c.city_id
+         inner join countries c2 on c.country_id = c2.country_id;
 
---
--- View structure for view `property_reviews_list`
---
+# view with all reviews for every property
+create view review_list as
+select l.listing_id, pr.user_id, pr.rating, pr.review_text
+from listings l
+         left join property_reviews pr on l.listing_id = pr.listing_id;
 
---
--- View structure for view `reservations_list`
---
+
+# view with a property reservation history
+create view property_reservation_history as
+select l.listing_id, r.start_date, r.end_date, t.total, u.first_name, u.last_name
+from listings l
+         left join reservations r on l.listing_id = r.listing_id
+         left join transactions t on r.transaction_id = t.transaction_id
+         inner join users u on r.user_id = u.user_id;
