970b22183c4a893c4566376127a865a7639b55ce
tmneth <80415416+tmneth@users.noreply.github.com>
2022-12-18 11:49:08 +0200

    Formatting fixes

--- airbnb-procedures.sql
+++ airbnb-procedures.sql
@@ -1,5 +1,5 @@
 # Deactivate user account:
-CREATE PROCEDURE deactivate_user (IN user_id INTEGER UNSIGNED)
+CREATE PROCEDURE deactivate_user(IN user_id INTEGER UNSIGNED)
 BEGIN
     UPDATE users
     SET active = FALSE
@@ -7,7 +7,7 @@ BEGIN
 END;
 
 # Activate user account:
-CREATE PROCEDURE activate_user (IN user_id INTEGER UNSIGNED)
+CREATE PROCEDURE activate_user(IN user_id INTEGER UNSIGNED)
 BEGIN
     UPDATE users
     SET active = TRUE
@@ -15,7 +15,7 @@ BEGIN
 END;
 
 # Set user's email address as verified:
-CREATE PROCEDURE verify_email (IN user_id INTEGER UNSIGNED)
+CREATE PROCEDURE verify_email(IN user_id INTEGER UNSIGNED)
 BEGIN
     UPDATE users
     SET email_verified = TRUE
@@ -24,16 +24,18 @@ END;
 
 # Update the country column to have the first letter capitalized when a new row is inserted into the countries table:
 CREATE TRIGGER update_country_case
-AFTER INSERT ON countries
-FOR EACH ROW
+    AFTER INSERT
+    ON countries
+    FOR EACH ROW
 BEGIN
     UPDATE countries SET country = CONCAT(UPPER(LEFT(NEW.country, 1)), SUBSTRING(NEW.country, 2)) WHERE id = NEW.id;
 END;
 
 # Update the city column to have the first letter capitalized when a new row is inserted into the countries table:
 CREATE TRIGGER update_country_case
-AFTER INSERT ON cities
-FOR EACH ROW
+    AFTER INSERT
+    ON cities
+    FOR EACH ROW
 BEGIN
     UPDATE cities SET city = CONCAT(UPPER(LEFT(NEW.city, 1)), SUBSTRING(NEW.city, 2)) WHERE id = NEW.id;
 END;
