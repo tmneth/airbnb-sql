569338b543053cfa202a16342043f1d6acb9f047
tmneth <80415416+tmneth@users.noreply.github.com>
2022-12-19 11:28:37 +0200

    Update procedures

--- airbnb-procedures.sql
+++ airbnb-procedures.sql
@@ -24,22 +24,4 @@ BEGIN
     WHERE id = user_id;
 END $$
 
-# Update the country column to have the first letter capitalized when a new row is inserted into the countries table:
-CREATE TRIGGER update_country_case
-    AFTER INSERT
-    ON countries
-    FOR EACH ROW
-BEGIN
-    UPDATE countries SET country = CONCAT(UPPER(LEFT(NEW.country, 1)), SUBSTRING(NEW.country, 2)) WHERE id = NEW.id;
-END $$
-
-# Update the city column to have the first letter capitalized when a new row is inserted into the countries table:
-CREATE TRIGGER update_country_case
-    AFTER INSERT
-    ON cities
-    FOR EACH ROW
-BEGIN
-    UPDATE cities SET city = CONCAT(UPPER(LEFT(NEW.city, 1)), SUBSTRING(NEW.city, 2)) WHERE id = NEW.id;
-END $$
-
 DELIMITER ;
